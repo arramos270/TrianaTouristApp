e87292b7d92f1804b54ba4682af51628ddbe083c
arramos270 <57353507+arramos270@users.noreply.github.com>
2021-12-17 13:35:51 +0100

    Subido proyecto generado

---
+++ src/main/java/com/example/RamirezRamosAlejandro_TrianaTouristApp/RamirezRamosAlejandroTrianaTouristAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.RamirezRamosAlejandro_TrianaTouristApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamirezRamosAlejandroTrianaTouristAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamirezRamosAlejandroTrianaTouristAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RamirezRamosAlejandro_TrianaTouristApp/RamirezRamosAlejandroTrianaTouristAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RamirezRamosAlejandro_TrianaTouristApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamirezRamosAlejandroTrianaTouristAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
